c8b1c69a8bf74921fad0b3f540360a3420511d51
Manfredi Lo Menzo <106279259+Manfrolm@users.noreply.github.com>
2023-10-14 18:11:22 +0200

    Add 09 - Components

---
+++ exercises/src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+const root = document.getElementById('root');
+
+const rootElement = ReactDOM.createRoot(root);
+
+rootElement.render(<App />);
