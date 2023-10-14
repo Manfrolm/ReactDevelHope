0c0fe74246fbc395029c54fb4d23440a36f682db
Manfredi Lo Menzo <106279259+Manfrolm@users.noreply.github.com>
2023-10-14 18:24:54 +0200

    Add 12 - Components

--- exercises/src/App.jsx
+++ exercises/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import { Hello }  from './components/Hello'
+import { Message } from './components/Message'
 
 function App() {
 
   return (
     <>
       <Hello />
+      <Message />
     </>
   )
 }
--- exercises/src/components/Hello.jsx
+++ exercises/src/components/Hello.jsx
@@ -1,8 +1,5 @@
 export function Hello() {
   return (
-    <>
       <h2>Hello, World!</h2>
-      <p>What a beautiful day!</p>
-    </>
   );
 }
