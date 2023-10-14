74f15c9cffe649302a3cfac7da572259d6dd5b45
Manfredi Lo Menzo <106279259+Manfrolm@users.noreply.github.com>
2023-10-14 17:29:23 +0200

    Add 03 - JSX

--- exercises/src/App.jsx
+++ exercises/src/App.jsx
@@ -3,11 +3,17 @@ import { PrintName } from './components/PrintName'
 
 function App() {
 
+  const name="Mario";
+
   return (
     <>
-      <PrintName name="Mario" />
+      <PrintName /> 
     </> 
   )
 }
 
 export default App
+
+
+{/* Nel caso in cui non dovessimo passare la variabile "name" alla funzione, il component verrà renderizzato comunque ma
+ma l'output del name sarà undefined, quindi "Hello, undefined"  e il nome non apparirà a schermo*/}
