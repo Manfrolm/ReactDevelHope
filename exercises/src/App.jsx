1fea5e32f8c8e831fd308d883763fb96ab1374ef
Manfredi Lo Menzo <106279259+Manfrolm@users.noreply.github.com>
2023-10-14 18:27:29 +0200

    Add 13 - Components

--- exercises/src/App.jsx
+++ exercises/src/App.jsx
@@ -7,9 +7,16 @@ function App() {
   return (
     <>
       <Hello />
+      <Hello />
+      <Hello />
+      <Hello />
+      <Hello />
       <Message />
     </>
   )
 }
 
 export default App
+
+
+{/* Il component, se utilizzato più volte, sarà renderizzato per tutte le volte che viene utilizzato (In questo caso avremo cinque tag h2) */}
