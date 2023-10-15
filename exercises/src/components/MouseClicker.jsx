b86b25e4be4f5b7472f6fa6e1dc9948bc2c1ad56
Manfredi Lo Menzo <106279259+Manfrolm@users.noreply.github.com>
2023-10-15 22:06:34 +0200

    Add 38 - Handling Events

--- exercises/src/components/MouseClicker.jsx
+++ exercises/src/components/MouseClicker.jsx
@@ -3,12 +3,21 @@ export function MouseClicker() {
 function handleButtonClick(e){
     console.log(e.currentTarget.name)
 }
+
+function handleImageClick(e){
+    console.log(e.currentTarget.src)
+}
     
 
 
     return (
       <>
-        <button onClick={handleButtonClick} name="one"><img src="https://www.quattrozampe.online/wp-content/uploads/2016/01/cane-felice.jpg" width={100} alt=""/>Click me!</button>
+        <button onClick={handleButtonClick} name="one"><img onClick={handleImageClick} src="https://www.quattrozampe.online/wp-content/uploads/2016/01/cane-felice.jpg" width={100} alt=""/>Click me!</button>
       </>
     );
   }
+
+
+  /* Se clicchiamo sull'immagine vedremo in console sia "name" che "src" perchè l'evento è associato al button (target) ma abbiamo azionato l'evento
+  cliccando sull'immagine (currentTarget), se clicchiamo, invece, sul pulsante e non sull'immagine, infatti, vedremo solo "name" 
+ */
