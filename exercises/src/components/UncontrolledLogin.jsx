0622948f8ff551ff236c0103da2b2cf802c0099a
Manfredi Lo Menzo <106279259+Manfrolm@users.noreply.github.com>
2023-10-21 13:37:39 +0200

    Add 48 - Uncontrolled forms

--- exercises/src/components/UncontrolledLogin.jsx
+++ exercises/src/components/UncontrolledLogin.jsx
@@ -5,25 +5,19 @@ export const UncontrolledLogin = () => {
     e.preventDefault();
 
 
-    // Metodo 1
-    // const username = e.target.username.value;
-    // const password = e.target.password.value;
-    // const remember = e.target.remember.checked;
+    
+    const username = e.target.username.value;
+    const password = e.target.password.value;
+    const remember = e.target.remember.checked;
 
-    // console.log(username);
-    // console.log(password);
-    // console.log(remember);
-
-    // Metodo 2
-    const formData = new FormData(e.target);
     const data = {
-      username: formData.get("username"),
-      password: formData.get("password"),
-      remember: formData.get("remember") === "on" ? true : false,
-    };
-
-    console.log(data);
-  }
+        username,
+        password,
+        remember
+      }
+      console.log(data);
+    }
+  
 
   return (
     <form onSubmit={handleSubmit}>
