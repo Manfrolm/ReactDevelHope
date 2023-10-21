aac047f4e8b71cb655ed25b763f95451f08419af
Manfredi Lo Menzo <106279259+Manfrolm@users.noreply.github.com>
2023-10-21 13:31:29 +0200

    Add 47 - Uncontrolled Forms

--- exercises/src/components/UncontrolledLogin.jsx
+++ exercises/src/components/UncontrolledLogin.jsx
@@ -1,22 +1,37 @@
-import React from 'react'
+import React from "react";
 
 export const UncontrolledLogin = () => {
+  function handleSubmit(e) {
+    e.preventDefault();
 
-    function handleSubmit(e){
-        e.preventDefault();
-   
-        alert("Successful Login")
-   }
-   
-   
+
+    // Metodo 1
+    // const username = e.target.username.value;
+    // const password = e.target.password.value;
+    // const remember = e.target.remember.checked;
+
+    // console.log(username);
+    // console.log(password);
+    // console.log(remember);
+
+    // Metodo 2
+    const formData = new FormData(e.target);
+    const data = {
+      username: formData.get("username"),
+      password: formData.get("password"),
+      remember: formData.get("remember") === "on" ? true : false,
+    };
+
+    console.log(data);
+  }
 
   return (
     <form onSubmit={handleSubmit}>
-        <input type="text" name="username" />
-        <input type="password" name="password" />
-        <input type="checkbox" name="remember" />
-        <button type="submit">Login</button>
-        <button type="reset">Reset</button>
+      <input type="text" name="username" />
+      <input type="password" name="password" />
+      <input type="checkbox" name="remember" />
+      <button type="submit">Login</button>
+      <button type="reset">Reset</button>
     </form>
-  )
-}
+  );
+};
