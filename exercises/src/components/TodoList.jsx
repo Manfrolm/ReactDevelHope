59c9608630c90370ebe42449f627bdde5cc9d453
Manfredi Lo Menzo <106279259+Manfrolm@users.noreply.github.com>
2023-10-22 11:44:15 +0200

    Add 59 - Lists and State

--- exercises/src/components/TodoList.jsx
+++ exercises/src/components/TodoList.jsx
@@ -5,33 +5,44 @@ export const TodoList = () => {
   const [items, setItems] = useState([]);
   const [todo, setTodo] = useState("");
 
-
   const handleChange = (e) => {
-    setTodo(e.target.value)
-  }
+    setTodo(e.target.value);
+  };
 
   const handleTodo = (e) => {
     setItems((items) => {
-        return [...items, todo];
-      });
-      setTodo("")
-  }
+      return [...items, todo];
+    });
+    setTodo("");
+  };
 
-  const handleReset = (e) => {
-    setItems([])
-  }
+  const handleReset = () => {
+    setItems([]);
+  };
 
+  const handleDeleteLi = (index) => {
+    const updatedItems = [...items];
+    updatedItems.splice(index, 1);
+    setItems(updatedItems);
+  };
 
   return (
     <>
       <ul>
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <div>
+            <li key={index}>{item}</li>
+            <button type="reset" onClick={() => handleDeleteLi(index)}>
+              Delete
+            </button>
+          </div>
         ))}
       </ul>
       <input type="text" value={todo} onChange={handleChange} />
       <button onClick={handleTodo}>Add</button>
-      <button type="reset" onClick={handleReset}>Reset</button>
+      <button type="reset" onClick={handleReset}>
+        Reset
+      </button>
     </>
   );
 };
