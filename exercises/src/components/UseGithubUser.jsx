061476d168dfb041835f3d1230a88960aee7f4f3
Manfredi Lo Menzo <106279259+Manfrolm@users.noreply.github.com>
2023-11-05 23:08:12 +0100

    Add 78 - Custom Hooks

--- exercises/src/components/UseGithubUser.jsx
+++ exercises/src/components/UseGithubUser.jsx
@@ -1,13 +1,15 @@
 import { useEffect } from "react";
 import { useState } from "react";
 
-export const UseGithubUser = (username) => {
+const UseGithubUser = (username) => {
   const API_URL = `https://api.github.com/users/${username}`;
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
+  const [loading,setLoading]= useState(false)
 
   const fetchData = async () => {
     try {
+      setLoading(true)
       const response = await fetch(API_URL);
       if (!response.ok) {
         throw new Error("Errore nella richiesta");
@@ -16,11 +18,15 @@ export const UseGithubUser = (username) => {
       setData(dataResponse);
     } catch (error) {
       setError(error);
+    }finally{
+      setLoading(false)
     }
   };
 
   useEffect(() => {
     fetchData();
   }, []);
-  return { data };
+  return { data, error, loading };
 };
+
+export default UseGithubUser
