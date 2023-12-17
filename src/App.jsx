ebf9baefed80b5c7fda370c0de01b2531ccb16b9
Manfredi Lo Menzo <106279259+Manfrolm@users.noreply.github.com>
2023-12-17 23:44:04 +0100

    add 85

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import {Routes, Route, Link} from "react-router-dom"
+import ShowGithubUser from "./ShowGithubUser";
+import { Welcome } from "./Welcome";
+import Counter from "./Counter";
+import NotFound from "./NotFound";
+import GithubUserList from "./GithubUserList";
+
+
+const App = () => {
+  return (
+    <>
+      <nav>
+        <Link to='/'>Home</Link>
+        <Link to='/counter'>Counter</Link>
+        <Link to='/users'>GithubUser</Link>
+      </nav>
+      <Routes>
+        <Route path="*" element={<NotFound/>} />
+        <Route path="/" element={<Welcome name="User"/>} />
+        <Route path="/counter" element={<Counter />}/>
+        <Route path="/users" element={<GithubUserList/>}>
+          <Route path=":username" element={<ShowGithubUser/>} />
+        </Route>
+      </Routes>
+    </>
+  )
+}
+
+export default App;
