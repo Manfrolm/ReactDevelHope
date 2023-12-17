7b1f75fc708bbc99cfac939046d9cf08a44569e0
Manfredi Lo Menzo <106279259+Manfrolm@users.noreply.github.com>
2023-12-17 23:46:13 +0100

    add 86

---
+++ src/GitHubUser.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+
+const API_URL = `https://api.github.com/users/`;
+
+const GitHubUser = ({ username }) => {
+  const [data, setData] = useState({});
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+        setError(false)
+      try {
+        const response = await fetch(`${API_URL}${username}`);
+        const result = await result.json();
+
+        if (response.ok) {
+          setData(result);
+        } else {
+          setError(result);
+        }
+      } catch (error) {
+        setError(error);
+      }
+    };
+
+    fetchData();
+  }, [username]);
+
+  return (
+    <div>
+      {error && <div>Error: {error.message}</div>}
+      {data && (
+        <div>
+          <div>Username: {data.login}</div>
+          <div>Name: {data.name}</div>
+          <div>
+            <img src={data.avatar_url} alt={`${data.login}'s avatar`} />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default GitHubUser;
